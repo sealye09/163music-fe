4376db21c437e1538a1c120f35415ee14e55e43b
SEAL <yejunhai2001@gmail.com>
2023-08-04 14:45:24 +0800

    fix: 重构艺术家详情页面，优化传参方式

--- src/pages/ArtistDetail/Albums.tsx
+++ src/pages/ArtistDetail/Albums.tsx
@@ -1,18 +1,44 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
 import Grid from "@/components/Grid";
 import Pagination from "@/components/Pagination";
 import { PlaylistInfo } from "@/types";
+import { artistApi } from "@/service";
 
 interface Props {
-  albums: PlaylistInfo[];
-  page: number;
-  pageSize: number;
-  setPage: Function;
-  albumSize: number;
+  artistId: string;
+  totalAlbums: number;
 }
 
-const Albums: FC<Props> = ({ albums, page, pageSize, setPage, albumSize }) => {
+const Albums: FC<Props> = ({ artistId, totalAlbums }) => {
+  const [albums, setAlbums] = useState<PlaylistInfo[]>([]);
+  const [page, setPage] = useState<number>(1);
+
+  const pageSize = 30;
+  // albums
+  useEffect(() => {
+    let isUnmounted = false;
+    if (isUnmounted) return;
+    artistApi.getArtistAlbums(artistId!, pageSize, (page - 1) * pageSize).then((res) => {
+      // @ts-ignore
+      const data = res.hotAlbums;
+      console.log("albums", res);
+
+      setAlbums(() =>
+        data.map((album: any) => {
+          return {
+            id: album.id,
+            name: album.name,
+            coverImgUrl: album.picUrl,
+          };
+        })
+      );
+    });
+
+    return () => {
+      isUnmounted = true;
+    };
+  }, [artistId, page]);
   return (
     <div>
       <Grid
@@ -20,10 +46,10 @@ const Albums: FC<Props> = ({ albums, page, pageSize, setPage, albumSize }) => {
         playlists={albums}
       />
       <div className="pt-10">
-        {albumSize !== 0 && (
+        {totalAlbums > pageSize && (
           <Pagination
             currPage={page}
-            totalPage={Math.floor(albumSize / pageSize) + 1}
+            totalPage={Math.floor(totalAlbums / pageSize) + 1}
             setPage={setPage}
           />
         )}
