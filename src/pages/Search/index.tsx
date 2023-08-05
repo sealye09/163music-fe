77fbdedf3b9012ae37bf99ab1e9d486fdf8b8113
SEAL <yejunhai2001@gmail.com>
2023-08-05 17:10:19 +0800

    fix: 重构搜索页面

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,13 +1,12 @@
 import { FC, useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { twMerge } from "tailwind-merge";
 
 import { PlaylistInfo, Track } from "@/types";
 import { searchApi } from "@/service";
+import Pagination from "@/components/Pagination";
 import TrackTable from "@/components/TrackTable";
 import Grid from "@/components/Grid";
-import Pagination from "@/components/Pagination";
-
-import "./index.css";
-import { useSearchParams } from "react-router-dom";
 
 interface AlbumInfo extends PlaylistInfo {}
 interface ArtistInfo extends PlaylistInfo {}
@@ -15,12 +14,37 @@ interface ArtistInfo extends PlaylistInfo {}
 // 搜索类型 1: 单曲, 10: 专辑, 100: 歌手, 1000: 歌单
 type SearchType = 1 | 10 | 100 | 1000;
 
+type Tab = {
+  id: SearchType;
+  name: string;
+  query: string;
+};
+
+const TabsCofig: Tab[] = [
+  {
+    id: 1,
+    name: "单曲",
+    query: "1",
+  },
+  {
+    id: 10,
+    name: "专辑",
+    query: "10",
+  },
+  {
+    id: 100,
+    name: "歌手",
+    query: "100",
+  },
+  {
+    id: 1000,
+    name: "歌单",
+    query: "1000",
+  },
+];
+
 const Search: FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-
-  const keywords = searchParams.get("keywords") || "";
-  const type = (Number(searchParams.get("type")) || 1) as SearchType;
-  const page = Number(searchParams.get("page")) || 1;
 
   const [songResult, setsongResult] = useState<Track[]>([]);
   const [albumResult, setAlbumResult] = useState<AlbumInfo[]>([]);
@@ -28,13 +52,45 @@ const Search: FC = () => {
   const [playlistResult, setPlaylistResult] = useState<PlaylistInfo[]>([]);
 
   const pageSize: number = 30;
+  const page = Number(searchParams.get("page")) || 1;
+  const type = Number(searchParams.get("type")) || 1;
+  const keywords = searchParams.get("keywords") || "";
 
   const searchAction = () => {
-    console.log("search", keywords);
-    searchApi.getSearchResult(keywords, 30, (page - 1) * pageSize, type).then((res) => {
+    console.log("search", keywords, "type", type);
+
+    if (searchParams.has("keywords")) {
+      searchParams.set("keywords", keywords);
+      setSearchParams(searchParams);
+    } else {
+      searchParams.append("keywords", keywords);
+      setSearchParams(searchParams);
+    }
+
+    if (keywords === "") {
+      setsongResult([]);
+      setAlbumResult([]);
+      setArtistResult([]);
+      return;
+    }
+
+    searchApi.getSearchResult(keywords, 30, (page - 1) * pageSize, type).then((res: any) => {
+      console.log(res);
+      if (
+        res.code !== 200 ||
+        res.result.songCount === 0 ||
+        res.result.albumCount === 0 ||
+        res.result.artistCount === 0 ||
+        res.result.playlistCount === 0
+      ) {
+        setsongResult([]);
+        setAlbumResult([]);
+        setArtistResult([]);
+        return;
+      }
+
       switch (type) {
         case 1:
-          // @ts-ignore
           const songs = res.result.songs;
           setsongResult(() =>
             songs.map((song: any) => {
@@ -58,7 +114,6 @@ const Search: FC = () => {
           break;
 
         case 10:
-          // @ts-ignore
           const albums = res.result.albums;
           setAlbumResult(() =>
             albums.map((album: any) => {
@@ -72,7 +127,6 @@ const Search: FC = () => {
           break;
 
         case 100:
-          // @ts-ignore
           const artists = res.result.artists;
           setArtistResult(() =>
             artists.map((artist: any) => {
@@ -86,7 +140,6 @@ const Search: FC = () => {
           break;
 
         default:
-          // @ts-ignore
           const playlists = res.result.playlists;
           setPlaylistResult(() =>
             playlists.map((playlist: any) => {
@@ -102,116 +155,93 @@ const Search: FC = () => {
   };
 
   const handleTypeChange = (type: SearchType) => {
-    setSearchParams({ type: type.toString() });
+    searchParams.set("type", type.toString());
+    searchParams.set("page", "1");
+    searchParams.set("keywords", keywords);
+    setSearchParams(searchParams);
   };
 
+  useEffect(() => {
+    searchAction();
+  }, [page, type]);
+
   return (
-    <div
-      className="w-full flex justify-center"
-      style={{
-        background: "#f5f5f5",
-      }}
-    >
-      <div
-        className="bg-white flex flex-col justify-center px-12"
-        style={{
-          width: "980px",
-        }}
-      >
-        <div className="search-bar py-12 flex justify-center">
-          <input
-            className="border w-80 h-9 rounded-l-2xl"
-            placeholder="搜索歌曲/歌手"
-            onChange={(e) => {
-              console.log(e.target.value);
-              // setKeywords(e.target.value);
-            }}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                searchAction();
-              }
-            }}
-          />
-          <button
-            className="border w-14 h-9 rounded-r-2xl text-sm"
-            onClick={searchAction}
-          >
-            搜索
-          </button>
-        </div>
-
-        <div
-          className="mb-6 flex items-center w-full h-12"
-          style={{
-            backgroundColor: "#f7f7f7",
-            border: "1px #d5d5d5 solid",
-          }}
-        >
-          <button
-            className={`btn-search min-w-fit h-12 pt-3 px-6 hover:bg-white hover:border-t-1 ${
-              type == 1 ? "btn-active" : ""
-            }`}
-            onClick={() => handleTypeChange(1)}
-          >
-            单曲
-          </button>
-          <button
-            className={`btn-search min-w-fit h-12 pt-3 px-6 hover:bg-white hover:border-t-1 ${
-              type == 10 ? "btn-active" : ""
-            }`}
-            onClick={() => handleTypeChange(10)}
-          >
-            专辑
-          </button>
-          <button
-            className={`btn-search min-w-fit h-12 pt-3 px-6 hover:bg-white hover:border-t-1 ${
-              type == 100 ? "btn-active" : ""
-            }`}
-            onClick={() => handleTypeChange(100)}
-          >
-            歌手
-          </button>
-          <button
-            className={`btn-search min-w-fit h-12 pt-3 px-6 hover:bg-white hover:border-t-1 ${
-              type == 1000 ? "btn-active" : ""
-            }`}
-            onClick={() => handleTypeChange(1000)}
-          >
-            歌单
-          </button>
-        </div>
-        {songResult.length !== 0 && type === 1 ? (
-          <div>
-            <TrackTable
-              listItems={["歌曲标题", "歌手", "专辑"]}
-              listInfo={songResult}
+    <div className="w-full flex justify-center bg-gray1">
+      <div className="bg-white w-content border border-x-gray1 flex flex-col justify-center px-12">
+        <div className="py-10 flex flex-col justify-center items-center gap-10">
+          <div className="flex justify-center">
+            <input
+              className="border w-80 h-9 rounded-l-2xl px-4 indent-2 text-sm focus:outline-0"
+              placeholder="搜索歌曲/歌手"
+              value={keywords}
+              onChange={(e) => {
+                console.log(e.target.value);
+                searchParams.set("keywords", e.target.value);
+                setSearchParams(searchParams);
+              }}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  searchAction();
+                }
+              }}
             />
+            <button
+              className="border w-14 h-9 rounded-r-2xl text-sm bg-gray1 hover:bg-white hover:border-t-1"
+              onClick={searchAction}
+            >
+              搜索
+            </button>
           </div>
-        ) : albumResult.length !== 0 && type === 10 ? (
-          <Grid
-            type={"album"}
-            playlists={albumResult}
-          />
-        ) : artistResult.length !== 0 && type === 100 ? (
-          <Grid
-            type={"artist"}
-            playlists={artistResult}
-          />
-        ) : playlistResult.length !== 0 && type === 1000 ? (
-          <Grid
-            type={"playlist"}
-            playlists={playlistResult}
-          />
-        ) : (
-          <div className="h-80"></div>
-        )}
 
-        <div className="py-10">
+          <div className="flex flex-col justify-center items-center gap-4 w-full">
+            <div className="flex items-center w-full h-12 bg-gray1 border border-gray1">
+              {
+                // @ts-ignore
+                TabsCofig.map((tab) => (
+                  <button
+                    key={tab.id}
+                    className={twMerge(
+                      "btn-search min-w-fit h-12 pt-3 px-6 hover:bg-white hover:border-t-2 hover:border-red-700",
+                      tab.id === type ? "border-t-2 border-red-700" : ""
+                    )}
+                    onClick={() => handleTypeChange(tab.id)}
+                  >
+                    {tab.name}
+                  </button>
+                ))
+              }
+            </div>
+            <div className="w-full">
+              {songResult.length !== 0 && type === 1 ? (
+                <TrackTable
+                  listItems={["歌曲标题", "歌手", "专辑"]}
+                  listInfo={songResult}
+                />
+              ) : albumResult.length !== 0 && type === 10 ? (
+                <Grid
+                  type={"album"}
+                  playlists={albumResult}
+                />
+              ) : artistResult.length !== 0 && type === 100 ? (
+                <Grid
+                  type={"artist"}
+                  playlists={artistResult}
+                />
+              ) : playlistResult.length !== 0 && type === 1000 ? (
+                <Grid
+                  type={"playlist"}
+                  playlists={playlistResult}
+                />
+              ) : (
+                <div className="h-80 flex justify-center items-center animate-pulse">
+                  <div className="text-2xl">Loading... </div>
+                </div>
+              )}
+            </div>
+          </div>
           <Pagination
             currPage={page}
             totalPage={10}
-            // setPage={setPage}
-            setPage={() => {}}
           />
         </div>
       </div>
